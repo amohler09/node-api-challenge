52885bea5b700a02868f269a064ed0c15d8dfdc1
Amber Mohler <ambermohler@iMac.local>
2020-03-13 11:48:25 -0400

    completed GET requests for projects/id and projects/id/actions

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -15,5 +15,37 @@ router.get('/', (req, res) => {
         })
 })
 
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    Projects.get(id)
+        .then(project => {
+            if(project) {
+                res.status(200).json(project);
+            } else {
+                res.status(404).json({ message: "Cannot find a project with that ID" });
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ message: "Error retrieving the project" });
+        })
+})
+
+router.get('/:id/actions', (req, res) => {
+    const id = req.params.id;
+    Projects.getProjectActions(id)
+        .then(actions => {
+            if (actions.length > 0) {
+                res.status(200).json(actions);
+            } else {
+                res.status(404).json({ message: "Cannot find any actions for the specified project ID" });
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ message: "Error retrieving actions" });
+        })
+})
+
 
 module.exports = router;
