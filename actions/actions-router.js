1f8472c04abf275a4e9cd4c151e3bda14de12bd5
Amber Mohler <ambermohler@iMac.local>
2020-03-13 12:38:59 -0400

    completed DELETE and PUT requests for actions, actions router is complete

--- actions/actions-router.js
+++ actions/actions-router.js
@@ -31,4 +31,37 @@ router.get('/:id', (req, res) => {
         })
 })
 
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    Actions.remove(id)
+        .then(action => {
+            if (!action) {
+                res.status(400).json({ message: "Invalid action ID" });
+            } else {
+                res.status(200).json({ message: "Action has been deleted" });
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ message: "Error deleting post" });
+        })
+})
+
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    if (req.body.id === id) {
+        Actions.update(id, req.body)
+        .then(action => {
+            res.status(200).json(action);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ message: "Error updating post" });
+        })
+    } else {
+        res.status(400).json({ message: "Make sure ID to update matches route and try again" });
+    }
+    
+})
+
 module.exports = router;
